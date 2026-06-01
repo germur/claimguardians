826e8c964a945e827232d82a94c74ab9ee12cfdc
Roger <rogermur1990@gmail.com>
2026-05-31 20:37:43 -0500

    fix SEO trailing slash + netlify config

--- apps/claimguardians-web/astro.config.mjs
+++ apps/claimguardians-web/astro.config.mjs
@@ -9,7 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://allclaimguardians.com',
-  trailingSlash: 'always',
+  trailingSlash: 'never',
   integrations: [react(), sitemap()],
 
   vite: {
